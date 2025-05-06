0431c3219ae98025982cecec925e98a77b07b793
Fedor Khamitov <fedor.khamitov@gmail.com>
2025-05-06 11:32:37 +0300

    Logging with Serilog & Exception Middware

---
+++ PetFamily.Backend/src/PetFamily.Api/Extensions/ExceptionMiddlewareExtentions.cs
@@ -0,0 +1,11 @@
+﻿using PetFamily.Api.Middleware;
+
+namespace PetFamily.Api.Extensions;
+
+public static class ExceptionMiddlewareExtentions
+{
+    public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ExceptionMiddleware>();
+    } 
+}
--- PetFamily.Backend/src/PetFamily.Api/Program.cs
+++ PetFamily.Backend/src/PetFamily.Api/Program.cs
@@ -1,16 +1,31 @@
+using PetFamily.Api.Extensions;
 using PetFamily.Application;
 using PetFamily.Infrastructure;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.Debug()
+    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq")!)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+    .CreateLogger();
+
+builder.Services.AddSerilog();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddInfrastructure()
     .AddApplication();
 
 var app = builder.Build();
+app.UseSerilogRequestLogging();
+app.UseExceptionMiddleware();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
